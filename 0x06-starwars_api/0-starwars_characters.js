0d14006c0de708b0f091184006e3faf8a0edd39d
clencyc <clency2023@gmail.com>
2024-09-08 19:48:06 +0300

    updated to axios

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,44 +1,31 @@
 #!/usr/bin/node
 
-
-const request = require('request');
+const axios = require('axios');
 
 if (process.argv.length < 3) {
-    console.error('Usage: node starwars_characters.js <Movie ID>');
+    console.error('Usage: node 0-starwars_characters.js <Movie ID>');
     process.exit(1);
 }
 
 const movieId = process.argv[2];
 const baseUrl = 'https://swapi.dev/api/films/';
 
-request(`${baseUrl}${movieId}/`, (error, response, body) => {
-    if (error) {
-        console.error('Error fetching movie data:', error);
-        return;
-    }
+axios.get(`${baseUrl}${movieId}/`)
+    .then(response => {
+        const filmData = response.data;
+        const characterUrls = filmData.characters;
 
-    if (response.statusCode !== 200) {
-        console.error('Failed to fetch movie data. Status code:', response.statusCode);
-        return;
-    }
-
-    const filmData = JSON.parse(body);
-    const characterUrls = filmData.characters;
-
-    characterUrls.forEach((url) => {
-        request(url, (error, response, body) => {
-            if (error) {
-                console.error('Error fetching character data:', error);
-                return;
-            }
-
-            if (response.statusCode !== 200) {
-                console.error('Failed to fetch character data. Status code:', response.statusCode);
-                return;
-            }
-
-            const characterData = JSON.parse(body);
-            console.log(characterData.name);
+        characterUrls.forEach(url => {
+            axios.get(url)
+                .then(response => {
+                    const characterData = response.data;
+                    console.log(characterData.name);
+                })
+                .catch(error => {
+                    console.error('Error fetching character data:', error);
+                });
         });
+    })
+    .catch(error => {
+        console.error('Error fetching movie data:', error);
     });
-});
