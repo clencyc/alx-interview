27702b234f02d1986c7cb1aedbdb4e9b447b5748
clencyc <clency2023@gmail.com>
2024-09-08 19:55:55 +0300

    semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,29 +3,29 @@
 const axios = require('axios');
 
 if (process.argv.length < 3) {
-    console.error('Usage: node 0-starwars_characters.js <Movie ID>');
-    process.exit(1);
+  console.error('Usage: node 0-starwars_characters.js <Movie ID>');
+  process.exit(1);
 }
 
 const movieId = process.argv[2];
 const baseUrl = 'https://swapi.dev/api/films/';
 
 axios.get(`${baseUrl}${movieId}/`)
-    .then(response => {
-        const filmData = response.data;
-        const characterUrls = filmData.characters;
+  .then(response => {
+    const filmData = response.data;
+    const characterUrls = filmData.characters;
 
-        characterUrls.forEach(url => {
-            axios.get(url)
-                .then(response => {
-                    const characterData = response.data;
-                    console.log(characterData.name);
-                })
-                .catch(error => {
-                    console.error('Error fetching character data:', error);
-                });
+    characterUrls.forEach(url => {
+      axios.get(url)
+        .then(response => {
+          const characterData = response.data;
+          console.log(characterData.name);
+        })
+        .catch(error => {
+          console.error('Error fetching character data:', error);
         });
-    })
-    .catch(error => {
-        console.error('Error fetching movie data:', error);
     });
+  })
+  .catch(error => {
+    console.error('Error fetching movie data:', error);
+  });
