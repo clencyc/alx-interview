316e426cee3f2d7e645f5f7d92c4683ae3309774
clencyc <clency2023@gmail.com>
2024-10-03 11:10:06 +0000

    Prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,51 +1,62 @@
 #!/usr/bin/python3
 
-def isWinner(x, nums):
-    if x <= 0 or not nums:
-        return None
+"""
+Sieve of Eratosthenes
+"""
 
-    max_num = max(nums)
-    primes = sieve_of_eratosthenes(max_num)
-    
+
+def isWinner(x, nums):
+    """
+    Primagame function
+    That determines the winner of the game
+    """
+    def sieve(n):
+        """ Use Sieve of Eratosthenes
+        to find all primes up to n """
+
+        is_prime = [True] * (n + 1)
+        p = 2
+        while (p * p <= n):
+            if (is_prime[p] is True):
+                for i in range(p * p, n + 1, p):
+                    is_prime[i] = False
+            p += 1
+        is_prime[0], is_prime[1] = False, False
+        return [p for p in range(n + 1) if is_prime[p]]
+
+    def game_winner(n):
+        """
+        Determines the winner
+        """
+
+        if n == 1:
+            return "Ben"
+
+        primes = sieve(n)
+        moves = 0
+        current_set = set(range(1, n + 1))
+
+        for prime in primes:
+            if prime in current_set:
+                moves += 1
+                multiples = set(range(prime, n + 1, prime))
+                current_set -= multiples
+
+        return "Maria" if moves % 2 != 0 else "Ben"
+
     maria_wins = 0
     ben_wins = 0
-    
-    for n in nums:
-        if play_game(n, primes):
+
+    for num in nums:
+        winner = game_winner(num)
+        if winner == "Maria":
             maria_wins += 1
         else:
             ben_wins += 1
-    
+
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
         return "Ben"
     else:
         return None
-
-def sieve_of_eratosthenes(max_num):
-    is_prime = [True] * (max_num + 1)
-    is_prime[0] = is_prime[1] = False
-    p = 2
-    while p * p <= max_num:
-        if is_prime[p]:
-            for i in range(p * p, max_num + 1, p):
-                is_prime[i] = False
-        p += 1
-    return [p for p in range(max_num + 1) if is_prime[p]]
-
-def play_game(n, primes):
-    numbers = set(range(1, n + 1))
-    turn = 0  # 0 for Maria, 1 for Ben
-    
-    while True:
-        move_made = False
-        for prime in primes:
-            if prime in numbers:
-                move_made = True
-                multiples = set(range(prime, n + 1, prime))
-                numbers -= multiples
-                break
-        if not move_made:
-            return turn == 1
-        turn = 1 - turn
